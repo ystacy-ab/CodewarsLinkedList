e2f20cf70ca79299f08e2e5abf82c71d83ea8c80
Anastasiia Yablunovska <yablunovska.pn@ucu.edu.ua>
2025-04-03 17:41:53 +0300

    Parse a linked list from a string 2

--- Parse_a_linked_list_from_a_string/parse_a_linked_list_from_a_string.py
+++ Parse_a_linked_list_from_a_string/parse_a_linked_list_from_a_string.py
@@ -1,6 +1,8 @@
 def linked_list_from_string(s):
     lst = s.split()
     ls = [f for f in lst if f != '->']
+    if ls[0] == "None":
+        return None
     result = Node(int(ls[0]))
     prev = result
     for i in range(1, len(ls) - 1):
